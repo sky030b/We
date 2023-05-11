34e513e9488acafc1aafdd4ef2323f3f1ece70b5
Sky <james156489723@gmail.com>
2023-05-11 10:37:39 +0800

    update all.js

--- js/all.js
+++ js/all.js
@@ -1,11 +1,27 @@
-$("#HeaderBtnBars").click(function () {
+// $("#HeaderBtnBars").click(function () {
+//     // alert(123)
+//     $("#HeaderBody").fadeIn();  
+//     $("body").css("overflow", "hidden")
+// })
+
+// $("#HeaderBtnXmark").click(function () {
+//     $("#HeaderBody").fadeOut();  
+//     $("body").css("overflow", "visible")
+// })
+
+const $HeaderBtnBars = $("#HeaderBtnBars")
+const $HeaderBtnXmark = $("#HeaderBtnXmark")
+const $HeaderBody = $("#HeaderBody")
+const $body = $("body")
+
+$HeaderBtnBars.click(function () {
     // alert(123)
-    $("#HeaderBody").fadeIn();  
-    $("body").css("overflow", "hidden")
+    $HeaderBody.fadeIn();  
+    $body.css("overflow", "hidden")
 })
 
-$("#HeaderBtnXmark").click(function () {
-    $("#HeaderBody").fadeOut();  
-    $("body").css("overflow", "visible")
+$HeaderBtnXmark.click(function () {
+    $HeaderBody.fadeOut();  
+    $body.css("overflow", "visible")
 })
 
